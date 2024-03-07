dbc0f2d84e3b2471feeae4e7e95c75486d6db830
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-03-07 22:39:01 +0530

    Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Jokes from './components/jokes/Jokes'
+
+function App() {
+
+
+  return (
+    <>
+    <Jokes/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/jokes/Jokes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Jokes.css"
+import Smiley from "../../assets/smiley.png"
+const Jokes = () => {
+  return (
+    <section className='jokes-sec'>
+    <div className='container joke'>
+    <h2>Jokes</h2>
+    <img src={Smiley} alt="smiley" />
+    <hr />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium delectus fugit at odit voluptatum optio hic enim perferendis quaerat neque.</p>
+    <button>Generate NEW JOKES</button>
+    </div>
+      
+    </section>
+  )
+}
+
+export default Jokes
